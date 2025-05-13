c06d6dad3b74fa18558a30eb8eaae9653e414ebf
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-13 17:01:23 -0300

    user_service - TokenService Configurado

---
+++ src/main/java/com/br/event_platform_backend/user_service/service/TokenService.java
@@ -0,0 +1,51 @@
+package com.br.event_platform_backend.user_service.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.br.event_platform_backend.user_service.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${security.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("auth-service")
+                    .withExpiresAt(generationExpirationDate())
+                    .withSubject(user.getEmail())
+                    .sign(algorithm);
+        }catch (JWTCreationException e){
+            return "Error while generating token!";
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-service")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            return "Error while validating token!";
+        }
+    }
+
+
+    private Instant generationExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
