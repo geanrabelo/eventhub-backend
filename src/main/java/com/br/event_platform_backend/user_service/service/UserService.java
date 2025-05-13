79ca3d9158f3cc539504d901dba52511bc89df1e
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-13 17:07:24 -0300

    user_service - SecurityFilter configurado

---
+++ src/main/java/com/br/event_platform_backend/user_service/service/UserService.java
@@ -0,0 +1,21 @@
+package com.br.event_platform_backend.user_service.service;
+
+import com.br.event_platform_backend.user_service.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
+}
